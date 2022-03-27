44473b6fc5f240fa036660501c7d4bcb9913320c
Mattia Gugole <78015843+MattiaGug@users.noreply.github.com>
2022-03-27 14:21:27 +0530

    Add files via upload

--- Calc.js
+++ Calc.js
@@ -1,1 +1,49 @@
+const display1E1 = document.querySelector('display-1');
+const display2E1 = document.querySelector('display-2');
+const tempResultE1 = document.querySelectorAll('.temp-result');
+const operationE1 = document.querySelectorAll('.operation')
+const numbersE1 = document.querySelectorAll('.operation');
+const equalE1 = document.querySelector('.equal');
+const clearE1 = document.querySelector('all-clear');
+const clearlastE1 = document.querySelector('last-entity-clear');
 
+let dis1Num = '';
+let dis2Num = '';
+let result = null;
+let lastOperation = '';
+let haveDot = false;
+
+
+/* se ho il punto la calcolatrice non deve aggiungerne un altro */
+
+numbersE1.forEach( number => {
+    number.addEventListener('click', (e) =>{
+        if(e.target.innerText === '.' && !haveDot){
+            haveDot = true;
+        } else if (e.target.innerText === '.' && haveDot){
+            return;
+        }
+        dis2Num += e.target.innerText;
+        display2E1.innerText = dis2Num
+    })
+})
+
+operationE1.forEach( operation => {
+    operation.addEventListener('click', (e) =>{
+        if(!dis2Num) result ;
+        haveDot= false;
+        const operationName = e.target.innerText;
+        if (dis1Num && dis2Num && lastOperation){
+            mathOperation();
+        } else {
+            result = parseFloat(dis2Num);
+        }
+        clearVar(operationName);
+        console.log(result);
+    })
+});
+
+function clearVar(name = ''){
+    dis1Num += dis2Num + ' ' + name + ' ';
+    display1E1.innerText = dis1Num;
+}
